6a306c96f29dbb35fe6cd5334471e6c11530764e
juanfel <jf3dsaber@gmail.com>
2014-08-31 02:01:23 -0400

    Actores se pueden mover en los 4 ejes del juego

---
+++ Assets/PlayerControlScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour {
+    //Se encarga de ver los inputs del jugador
+    public RadialMovementScript movementScript;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            movementScript.moveSideways(Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            movementScript.moveForward(Input.GetAxis("Vertical"));
+        }
+	}
+}
---
+++ Assets/RadialMovementScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadialMovementScript : MonoBehaviour {
+
+    //Se encarga de dar las funciones de movimiento radial a las distintos
+    //tipos de naves
+    public float initial_distance;
+    public float theta;
+    public float radius = 4;
+    public float speed; //en angulos por update
+    public float linealSpeed; //En unidades por update 
+    const float MAX_ANGLE = 2f*Mathf.PI;
+    const float MAX_SPEED = 5f;
+    Quaternion angleQuat;
+    Vector3 position;
+    // Use this for initialization
+    void Start()
+    {
+        //Ajusta el sprite inicial al cilindro donde debe moverse
+        position = radius*transform.position.normalized;
+        position.z = initial_distance;
+        theta = Vector3.Angle(Vector3.right,position); //En grados
+        theta = theta * Mathf.Deg2Rad;
+        //Con el producto punto se obtiene la direccion relativa del vector posicion
+        //con respecto a 0
+        theta *= Mathf.Sign(Vector3.Cross(Vector3.right, position).z); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position;
+    }
+   
+    void FixedUpdate()
+    {
+        
+    }
+    //moveLeft y moveRight mueven a la nave en la direccion indicada, como
+    //si la nave estuviese en un circulo. Esto implica que si la nave esta dada vuelta 
+    //se va a mover en la direccion contraria
+    public void moveLeft()
+    {
+        moveSideways(1f); 
+    }
+    public void moveRight()
+    {
+        moveSideways(-1f);
+    }
+    public void moveSideways(float axis)
+    {
+        theta = (theta + Mathf.Sign(axis)*speed) % MAX_ANGLE;
+        position.x = radius * Mathf.Cos(theta);
+        position.y = radius * Mathf.Sin(theta);
+    }
+    public void moveForward(float axis)
+    {
+        //Acerca o aleja al actor de la camara
+        position.z = (position.z + Mathf.Sign(axis) * linealSpeed);
+        position.z = Mathf.Clamp(position.z, 0, initial_distance);
+    }
+    public void moveForward()
+    {
+        //Alias para que actor avanze hacia la camara
+        moveForward(1f);
+    }
+}
