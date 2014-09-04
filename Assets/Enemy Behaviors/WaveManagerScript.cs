0c3b687302017c2dc04e3cd73dd5b97445726ae7
juanfel <jf3dsaber@gmail.com>
2014-09-04 13:47:29 -0400

    Ahora enemigos atacan en forma randomica

--- Assets/Enemy Behaviors/WaveManagerScript.cs
+++ Assets/Enemy Behaviors/WaveManagerScript.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class WaveManagerScript : MonoBehaviour {
     //Se encarga de monitorear el comportamiento de todas las naves de una wave
     //dandoles las instrucciones para moverse adelante o no
@@ -8,12 +8,17 @@ public class WaveManagerScript : MonoBehaviour {
     bool attackOrder = false;
     bool invokedAttack = false;
     public GameObject player;
+    List<EnemyAIBehavior> itemsForRandomEnable;
 
     float spawnTime;
-    const float MAXSPAWNTIME = 10f;
+    const float MAXSPAWNTIME = 4f;
  
 	void Start () {
-        
+        itemsForRandomEnable = new List<EnemyAIBehavior>();
+        foreach (Transform child in transform)
+        {
+            itemsForRandomEnable.Add(child.gameObject.GetComponent<EnemyAIBehavior>());
+        }
 	}
 	
 	// Update is called once per frame
@@ -22,23 +27,42 @@ public class WaveManagerScript : MonoBehaviour {
         //de naves en un tiempo arbitrario
         if(!invokedAttack)
         {
-            spawnTime = Random.Range(5,MAXSPAWNTIME);
+            spawnTime = Random.Range(0,MAXSPAWNTIME);
             Invoke("GiveAttackOrder",spawnTime);
             invokedAttack = true;
         }
         if (attackOrder)
         {
-            foreach (Transform enemy in transform)
-            {
-                enemy.gameObject.GetComponent<ZigZaggingAiBehavior>().SetTarget(player);
-                enemy.gameObject.GetComponent<ZigZaggingAiBehavior>().StartMovement();
-                //StartCoroutine(StartMovement(enemy.gameObject));
+            //foreach (Transform enemy in transform)
+            //{
+            //    enemy.gameObject.GetComponent<ZigZaggingAiBehavior>().SetTarget(player);
+            //    enemy.gameObject.GetComponent<ZigZaggingAiBehavior>().StartMovement();
+            //    //StartCoroutine(StartMovement(enemy.gameObject));
 
-            }
+            //}
+            GiveRandomAttackOrder();
             attackOrder = false ;
         }
         
 	}
+    void GiveRandomAttackOrder()
+    {
+        //Da una orden de ataque a un grupo aleatorio de enemigos
+        int count = itemsForRandomEnable.Count;
+        Shuffle<EnemyAIBehavior>(itemsForRandomEnable);
+        if (itemsForRandomEnable != null && count > 0)
+        {
+            int itemId = new System.Random().Next(itemsForRandomEnable.Count);
+            
+            for (int i = 0; i < itemsForRandomEnable.Count; i++)
+            {
+                itemsForRandomEnable[i].SetTarget(player);
+                if (i == itemId) { itemsForRandomEnable[i].StartMovement(); }
+               
+            }
+
+        }
+    }
     void GiveAttackOrder()
     {
         Debug.Log("ATTACK!");
@@ -50,5 +74,18 @@ public class WaveManagerScript : MonoBehaviour {
         //Empieza a mover a los hijos dentro del rango dado
         yield return new WaitForSeconds(Random.Range(0, 4));
         enemy.gameObject.GetComponent<EnemyAIBehavior>().StartMovement();
+    }
+    //Funciones de utilidad para 
+    public static void Shuffle<T>(List<T> list)
+    {
+        for (var i = 0; i < list.Count; i++)
+            Swap(list,i, Random.Range(i, list.Count));
+    }
+
+    public static void Swap<T>(List<T> list, int i, int j)
+    {
+        var temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
     }
 }
