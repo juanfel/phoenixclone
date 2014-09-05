20aca3076fa0f251da1a1fac13b4e5d0c440e93c
juanfel <jf3dsaber@gmail.com>
2014-09-05 00:14:34 -0400

    Agregados menu principal y botones para iniciar el juego

---
+++ Assets/MainScreenButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScreenButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        GUI.Box(new Rect(
+            ((int)Screen.width*0.4f),((int)Screen.height*0.2f), 
+            ((int)Screen.width*0.2f), ((int)Screen.height*0.4f)), "NeoPhoenix");
+        if (GUI.Button(new Rect(
+            ((int)Screen.width*0.45f),((int)Screen.height*0.4f), 
+            ((int)Screen.width*0.1f), ((int)Screen.height*0.1f)), "Play"))
+        {
+            Application.LoadLevel("gameplay");
+        }
+        if (GUI.Button(new Rect(
+            ((int)Screen.width*0.45f),((int)Screen.height*0.5f), 
+            ((int)Screen.width*0.1f), ((int)Screen.height*0.1f)), "Credits")) 
+        {
+            Debug.Log("Credits?");
+        }
+    }
+}
