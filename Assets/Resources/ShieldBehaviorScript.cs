32fbe1fd3c609b6913fcb953e5cc620e5ea01742
juanfel <jf3dsaber@gmail.com>
2014-09-09 23:52:47 -0300

    Ahora el escudo funciona bien

--- Assets/Resources/ShieldBehaviorScript.cs
+++ Assets/Resources/ShieldBehaviorScript.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class ShieldBehaviorScript : MonoBehaviour {
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+       
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update()
+    {
+        if (transform.parent.collider.enabled)
+        {
+            transform.parent.collider.enabled = false;
+        }
+    }
     void OnCollisionEnter(Collision coll)
     {
         //Evita que colisione con el que creo el escudo
@@ -18,18 +22,11 @@ public class ShieldBehaviorScript : MonoBehaviour {
         {
             Physics.IgnoreCollision(transform.collider, coll.collider);
         }
-        else
-        {
-            //Hace que cualquier otra entidad no pueda tocar al jugador hasta que salga
-            Physics.IgnoreCollision(transform.parent.collider, coll.collider,true);
-        }
+        
     }
-    void OnCollisionExit(Collision coll)
+    void OnDestroy()
     {
-        //Recupera la collision entre el jugador y quien sea que toco el shield
-        if (transform.parent.tag != coll.gameObject.tag)
-        {
-            Physics.IgnoreCollision(transform.parent.collider, coll.collider,false);
-        }
+        transform.parent.collider.enabled = true;
     }
+    
 }
