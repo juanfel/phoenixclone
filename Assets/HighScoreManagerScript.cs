ab168ade4d8d8f720ffb666be4c75141b63dd6b3
juanfel <jf3dsaber@gmail.com>
2014-09-13 02:22:09 -0300

    Creado script para setear highscore

---
+++ Assets/HighScoreManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreManagerScript : MonoBehaviour {
+    GameObject GuiManager;
+    GUIText scoretext;
+    GUIText highscoretext;
+    bool isHighscore = false;
+	// Use this for initialization
+	void Start () {
+        GuiManager = GameObject.FindGameObjectWithTag("GuiManager");
+        scoretext = GuiManager.GetComponent<PersistenScoreScript>().getScoreText();
+        if(int.Parse(scoretext.text) > int.Parse(highscoretext.text))
+        {
+            isHighscore = true;
+            highscoretext.text = scoretext.text;
+            scoretext.text = "0";
+        }
+        else
+        {
+            Application.LoadLevel("mainmenu");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    void onGui()
+    {
+
+    }
+}
