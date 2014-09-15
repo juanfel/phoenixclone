3b86764f2f6ac1ea11927465dd8216a8f58a4049
juanfel <jf3dsaber@gmail.com>
2014-09-14 22:34:31 -0300

    Creada clase que genera powerups

---
+++ Assets/Resources/EnemyBehaviors/PowerUpDroppingBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpDroppingBehavior : MonoBehaviour {
+    //Se encarga de que el enemigo dropee powerups. Tira un dado para ver si corresponde
+    public string powerUpName;
+	public void DropPowerUp()
+    {
+        int chance = Random.Range(0, 6);
+        if(chance > 4)
+        {
+            GameObject powerUp = (GameObject)Resources.Load(powerUpName);
+            Instantiate(powerUp);
+        }
+    }
+}
