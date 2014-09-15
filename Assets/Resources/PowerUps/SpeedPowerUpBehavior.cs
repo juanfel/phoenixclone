4b479657f3980f15dc18a6a0e987b5e62e11b801
juanfel <jf3dsaber@gmail.com>
2014-09-14 22:29:57 -0300

    Crea un powerup para la velocidad

---
+++ Assets/Resources/PowerUps/SpeedPowerUpBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPowerUpBehavior : EnemyBulletForwardBehavior {
+    //Crea un powerup que aumenta la velocidad de la nave
+    public float speedAugment;
+    public float powerUpTime;
+    RadialMovementScript player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    protected override void Hit(Collision coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            player = coll.gameObject.GetComponent<RadialMovementScript>();
+            player.speed += speedAugment;
+            Invoke("RevertChange", powerUpTime);
+            
+        }
+    }
+    void RevertChange()
+    {
+        if(player)
+        {
+            player.speed -= speedAugment;
+        }
+    }
+}
