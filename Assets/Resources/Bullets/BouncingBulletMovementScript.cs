99dbbefe2def5acde6941144a19da38644e89bdf
juanfel <jf3dsaber@gmail.com>
2014-09-23 22:23:56 -0300

    Creada bala que rebota cuando choca con cualquier cosa que no sea el player

---
+++ Assets/Resources/Bullets/BouncingBulletMovementScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncingBulletMovementScript : DiagonalBulletMovementScript {
+
+    protected override void Hit(Collision coll)
+    {
+        //Esta bala debe chocar con cualquier enemigo y cambiar su direccion si eso ocurre
+        if(coll.gameObject.tag != "player")
+        {
+            setMoveLeft(!moveLeft);
+        }
+        base.Hit(coll);
+    }
+}
