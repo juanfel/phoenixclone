e2fabba304f885de17439492dd73ed05914ab9a3
juanfel <jf3dsaber@gmail.com>
2014-09-23 22:32:08 -0300

    Creado BouncingShotBehavior

---
+++ Assets/Resources/EnemyBehaviors/BouncingShotBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncingShotBehavior : BaseShotBehavior {
+    //Dispara balas rebotantes
+    string tipo_disparo = "Bullets/BouncingEnemyBullet";
+    GameObject disparo;
+    BouncingBulletMovementScript currentShot;
+    float rateOfFire = 2f;
+    // Use this for initialization
+    void Start()
+    {
+        disparo = (GameObject)Resources.Load(tipo_disparo);
+    }
+
+
+    public override void Shot()
+    {
+        currentShot = disparo.GetComponent<BouncingBulletMovementScript>();
+
+        currentShot.owner = gameObject;
+        currentShot.owner_tag = gameObject.tag;
+        currentShot.direction = -1;
+        int chance = Random.Range(0,1);
+        if (chance > 0)
+            currentShot.setMoveLeft(true);
+        else
+            currentShot.setMoveLeft(false);
+        Instantiate(disparo, gameObject.transform.position, Quaternion.identity);
+        Invoke("ReadyShot", rateOfFire);
+        shotReady = false;
+    }
+}
